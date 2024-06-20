3a78d1366a64701aa66f238106c0f17619367694
Nguyen Minh Phi <nmphi2710@gmail.com>
2024-06-20 15:11:06 +0700

    feat(server): connect to MongoDB database

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+// CONNECT TO DATABASE
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB).then(() => console.log('Database connection successful!'));
 
 const port = process.env.PORT;
 const server = app.listen(port, () => {
