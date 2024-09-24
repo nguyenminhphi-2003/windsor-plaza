d4538c48bb7e8b25b248b067ffdbd7f13370245f
Nguyen Minh Phi <nmphi2710@gmail.com>
2024-09-24 15:44:02 +0700

    feat(client): finish footer about session

--- client/src/user/components/Footer.jsx
+++ client/src/user/components/Footer.jsx
@@ -12,15 +12,15 @@ export default function Footer() {
     <footer className="absolute bottom-0">
       <About />
       <Contact />
-      <div>Policy</div>
+      <Policy />
     </footer>
   );
 }
 
 function About() {
   return (
-    <div className="footer-about flex">
-      <div className="about-logo inline-block w-24 h-24">
+    <div className="footer-about flex bg-primary-red border-b border-primary-gold">
+      <div className="about-logo inline-block w-24 h-24 shrink-0 mr-5">
         <img
           className="w-full h-full ml-2.5 mr-2.5 mb-2.5"
           src={appLogo}
@@ -28,29 +28,29 @@ function About() {
         />
       </div>
 
-      <div className="about-information">
-        <h2>Windsor Plaza Hotel</h2>
+      <div className="about-information mx-5 my-4 ml-8 text-primary-gold w-auto shrink-0">
+        <div className='font-medium uppercase text-2xl mb-4'>Windsor Plaza Hotel</div>
         <ul>
-          <li>
+          <li className='text-lg mb-1'>
             <FontAwesomeIcon icon={faLocationDot} /> 1 Su Van Hanh Street,
             District 5, Ho Chi Minh City, Vietnam
           </li>
-          <li>
+          <li className='text-lg mb-1'>
             <FontAwesomeIcon icon={faPhone} /> +84 28 8888 8888
           </li>
-          <li>
+          <li className='text-lg mb-1'>
             <FontAwesomeIcon icon={faEnvelope} /> service@windsorplazahotel.com
           </li>
         </ul>
       </div>
 
-      <div className="about-lisence">
-        <h2>
+      <div className="about-lisence w-auto text-white mx-6 my-6 text-lg font-bold">
+        <h2 className='mb-3'>
           CHI NHÁNH CÔNG TY CỔ PHẦN TẬP ĐOÀN ĐẦU TƯ AN ĐÔNG – KHÁCH SẠN THƯƠNG
           MẠI AN ĐÔNG
         </h2>
 
-        <p>
+        <p className='text-lg'>
           Giấy CNĐKDN: 0304938912-001 – Ngày cấp: 30/06/2008 (Sửa đổi lần thứ 4,
           ngày 05/10/2017) Cơ quan cấp: Phòng Đăng ký kinh doanh – Sở kế hoạch
           và Đầu tư TP.HCM Địa chỉ đăng ký kinh doanh: 18 An Dương Vương, Phường
@@ -95,5 +95,10 @@ function Contact() {
 }
 
 function Policy() {
-  return <div className="footer-policy"></div>;
+  return (
+    <div className="footer-policy flex">
+      <p>Terms & Conditions</p>
+      <p>Privacy</p>
+    </div>
+  );
 }
